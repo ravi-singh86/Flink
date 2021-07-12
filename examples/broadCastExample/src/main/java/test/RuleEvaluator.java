b5e4f68d95ae9d36e3bdef1b609422fc207e4ebb
Ravi <shanker86@gmail.com>
2021-07-11 20:54:32 -0400

    added a demo rule evaluator skelton

---
+++ examples/broadCastExample/src/main/java/test/RuleEvaluator.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Gauge;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+
+
+public class RuleEvaluator extends BroadcastProcessFunction<Integer,String,String> {
+
+	
+	@Override
+	public void processElement(Integer event,
+				  			   ReadOnlyContext ctx,
+							   Collector<String> out)
+	{
+		System.out.println("The event="+event);
+	}
+
+	@Override
+	public void processBroadcastElement(String rule, Context ctx, Collector<String> out)
+	{
+		System.out.println("The rule="+rule);
+	}
+}
---
+++ examples/broadCastExample/src/main/java/test/RulePipeline.java
@@ -0,0 +1,84 @@
+package test;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.functions.MapFunction;
+
+import test.RuleEvaluator;
+
+public class RulePipeline {
+
+    public static void main(String[] args) throws Exception {
+
+    
+		
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		DataStream<String>  ruleStream = env.socketTextStream("localhost",9991);
+		DataStream<Integer> dataStream = env.socketTextStream("localhost",9992)
+											.map(new MapFunction<String, Integer>(){
+												@Override
+												public Integer map(String value) throws Exception {
+													return Integer.parseInt(value);
+												}
+											});
+
+		MapStateDescriptor<String, String> ruleStateDescriptor = new MapStateDescriptor<>(
+			"RulesBroadcastState",
+			BasicTypeInfo.STRING_TYPE_INFO,
+			BasicTypeInfo.STRING_TYPE_INFO
+			);
+
+
+		BroadcastStream<String> ruleBroadcastStream = ruleStream.broadcast(ruleStateDescriptor);
+
+
+		DataStream<String> output = dataStream.connect(ruleBroadcastStream)
+											  .process(new RuleEvaluator());
+
+
+        
+
+        output.writeAsText("test.txt");
+
+        env.execute("WindowRuleMatching");
+		
+
+		/*
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+				//.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+        dataStream.writeAsText("test.txt");
+
+        env.execute("Window WordCount");
+		*/
+
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+
+}
+
